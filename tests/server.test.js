79c717fba3a854c4adb72b6fe9b079ed69967667
Zaine Kingi <rous6026nz@gmail.com>
2018-08-23 21:05:05 +1200

    Add app data to package.json file and server.test structure.

---
+++ tests/server.test.js
@@ -0,0 +1,17 @@
+// 
+// Server test file.
+// 
+
+/* 
+    File dependencies.
+    -----------------------------------
+    *** BASE DEPENDENCIES ***
+    - Application server file
+
+    *** CUSTOM DEPENDENCIES ***
+    Add any other dependencies here:
+*/
+
+// 
+// Server Test suite.
+// 
